fdf4f24e2e4817c974282bd3fb36f08de15dfd50
Brianna <bri@songoda.com>
2019-07-14 20:42:41 -0400

    Weird bug fix.

--- src/main/java/com/songoda/epicfurnaces/utils/Methods.java
+++ src/main/java/com/songoda/epicfurnaces/utils/Methods.java
@@ -88,14 +88,13 @@ public class Methods {
         if (location == null)
             return "";
         String w = location.getWorld().getName();
-        double x = location.getX();
-        double y = location.getY();
-        double z = location.getZ();
+        double x = location.getBlockX();
+        double y = location.getBlockY();
+        double z = location.getBlockZ();
         String str = w + ":" + x + ":" + y + ":" + z;
-        str = str.replace(".0", "").replace(".", "/");
+        str = str.replace(".0", "").replace("/", "");
         return str;
     }
-
     private static Map<String, Location> serializeCache = new HashMap<>();
 
     /**
